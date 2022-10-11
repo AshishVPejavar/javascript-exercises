94410eaac0f18939cd967310f6e8bdf824bc3469
Ashish Pejavar <ashishpejavar@gmail.com>
2022-10-11 16:55:02 -0400

    js ex3 done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,28 @@
-const reverseString = function() {
+const reverseString = function(text) {
 
-};
+    let textBox = text;
+    
+
+    if (textBox){
+    let reverseTextBox = "";
+        for(let a = textBox.length; a >= 0 ; a-- ){
+            if(reverseTextBox == "undefined"){
+                reverseTextBox = textBox[a];
+            } else {
+                reverseTextBox += textBox[a];
+            }
+           
+             
+             
+            }
+    return reverseTextBox; 
+    }
+    
+    else {return "";}
+
+   
+    }
+;
 
 // Do not edit below this line
 module.exports = reverseString;
